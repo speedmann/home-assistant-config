5bbba5981564b500ba0c440553b4dd2c3561a5f5
speedmann <speedmann@speedmann.de>
2018-10-03 18:40:22 +0200

    Copy all configs...

--- git-pull.sh
+++ git-pull.sh
@@ -2,6 +2,6 @@ apt update && apt install -y git || exit 1
 
 git clone https://github.com/speedmann/home-assistant-config /tmp/config
 
-cp -r /tmp/config/config/*.yaml /config/
+cp -r /tmp/config/config/ /config/
 
 rm -rf /tmp/config
